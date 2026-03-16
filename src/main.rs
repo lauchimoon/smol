175160ce0df22a973d73b8dff2accc3661dd39f4
lauchimoon <laurentino.luna06@gmail.com>
2026-03-16 10:41:35 -0300

    Improve error messages in main

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,17 @@
 use smol::lexer::Lexer;
 use smol::parser::Parser;
 use smol::interpreter::Interpreter;
+use smol::format;
 use std::fs;
 use std::env;
+use std::process;
 
 fn main() {
     let args: Vec<_> = env::args().collect();
+    if args.len() < 2 {
+        error("no input file");
+    }
+
     let filename = args[1].clone();
     let source = read_file(&filename);
     let tokens = Lexer::new(&filename, source).lex();
@@ -16,5 +22,16 @@ fn main() {
 }
 
 fn read_file(filepath: &str) -> String {
-    fs::read_to_string(String::from(filepath)).expect("error opening file")
+    match fs::read_to_string(String::from(filepath)) {
+        Ok(src) => src,
+        Err(_) => {
+            error(format!("file '{filepath}' not found").as_str());
+            unreachable!()
+        }
+    }
+}
+
+fn error(msg: &str) {
+    eprintln!("{}: {}: {msg}", format::bold("smol"), format::error("error"));
+    process::exit(1);
 }
